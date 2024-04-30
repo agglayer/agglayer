8bec8faa5bee6460f5441234d4f0c3a1efda5ad8
Simon Paitrault <simon.paitrault@gmail.com>
2024-04-30 10:09:27 +0200

    feat(config): adding json format support to logger
    Signed-off-by: Simon Paitrault <simon.paitrault@gmail.com>

--- src/logging.rs
+++ src/logging.rs
@@ -1,17 +1,24 @@
 use tracing_subscriber::{prelude::*, util::SubscriberInitExt, EnvFilter};
 
+use crate::config::log::LogFormat;
+
 pub(crate) fn tracing(config: &crate::config::Log) {
     // TODO: Support multiple outputs.
     let writer = config.outputs.first().cloned().unwrap_or_default();
 
-    tracing_subscriber::Registry::default()
-        .with(
-            tracing_subscriber::fmt::layer()
-                .pretty()
-                .with_writer(writer.as_make_writer())
-                .with_filter(
-                    EnvFilter::try_from_default_env().unwrap_or_else(|_| config.level.into()),
-                ),
-        )
-        .init();
+    let layer = match config.format {
+        LogFormat::Pretty => tracing_subscriber::fmt::layer()
+            .pretty()
+            .with_writer(writer.as_make_writer())
+            .with_filter(EnvFilter::try_from_default_env().unwrap_or_else(|_| config.level.into()))
+            .boxed(),
+
+        LogFormat::Json => tracing_subscriber::fmt::layer()
+            .json()
+            .with_writer(writer.as_make_writer())
+            .with_filter(EnvFilter::try_from_default_env().unwrap_or_else(|_| config.level.into()))
+            .boxed(),
+    };
+
+    tracing_subscriber::Registry::default().with(layer).init();
 }
