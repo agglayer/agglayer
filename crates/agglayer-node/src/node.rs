980a38fd1d682d56415a2245a4082f960a5173c4
Simon Paitrault <simon.paitrault@gmail.com>
2024-05-07 23:45:24 +0200

    feat(clock): adding clock crate to manage epoch duration
    Signed-off-by: Simon Paitrault <simon.paitrault@gmail.com>

--- crates/agglayer-node/src/node.rs
+++ crates/agglayer-node/src/node.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
-use agglayer_config::Config;
+use agglayer_clock::{Clock, TimeClock};
+use agglayer_config::{Config, Epoch};
 use anyhow::Result;
 use ethers::{
     middleware::MiddlewareBuilder as _,
@@ -46,6 +47,7 @@ impl Node {
     /// - The L1 node URL is invalid.
     /// - The configured signer is invalid.
     /// - The RPC server failed to start.
+    /// - The time clock failed to start.
     #[builder(entry = "builder", exit = "start", visibility = "pub(crate)")]
     pub(crate) async fn start(config: Arc<Config>) -> Result<()> {
         // Create a new L1 RPC provider with the configured signer.
@@ -54,6 +56,17 @@ impl Node {
 
         // Construct the core.
         let core = Kernel::new(rpc, config.clone());
+
+        // Spawn the time clock.
+        let (_clock_ref, _epoch_ref) = match &config.epoch {
+            Epoch::TimeClock(cfg) => {
+                let clock = TimeClock::new_now(cfg.epoch_duration.as_secs());
+
+                let epoch_ref = clock.epoch_ref();
+
+                (clock.spawn().await?, epoch_ref)
+            }
+        };
 
         // Bind the core to the RPC server.
         let server_handle = AgglayerImpl::new(core).start(config).await?;
