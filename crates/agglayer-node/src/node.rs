2e86f233320d2e8773cd5550f9c6ad755283d249
Simon Paitrault <simon.paitrault@gmail.com>
2024-05-07 21:26:43 +0200

    refactor(node): split tokio runtimes for metrics and node
    Signed-off-by: Simon Paitrault <simon.paitrault@gmail.com>

---
+++ crates/agglayer-node/src/node.rs
@@ -0,0 +1,65 @@
+use std::sync::Arc;
+
+use agglayer_config::Config;
+use anyhow::Result;
+use ethers::{
+    middleware::MiddlewareBuilder as _,
+    providers::{Http, Provider},
+};
+
+use crate::{kernel::Kernel, rpc::AgglayerImpl};
+
+pub(crate) struct Node {}
+
+#[buildstructor::buildstructor]
+impl Node {
+    /// Function that setups and starts the Agglayer node.
+    ///
+    /// The available methods are:
+    ///
+    /// - `builder`: Creates a new builder instance.
+    /// - `config`: Sets the configuration.
+    /// - `start`: Starts the Agglayer node.
+    ///
+    /// # Examples
+    /// ```no_run
+    /// # use std::sync::Arc;
+    /// # use agglayer_config::Config;
+    /// # use agglayer_node::Node;
+    /// # use anyhow::Result;
+    /// #
+    /// async fn start_node() -> Result<()> {
+    ///    let config: Arc<Config> = Arc::new(Config::default());
+    ///
+    ///    Node::builder()
+    ///      .config(config)
+    ///      .start()
+    ///      .await?;
+    ///
+    ///    Ok(())
+    /// }
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if:
+    /// - The L1 node URL is invalid.
+    /// - The configured signer is invalid.
+    /// - The RPC server failed to start.
+    #[builder(entry = "builder", exit = "start", visibility = "pub(crate)")]
+    pub(crate) async fn start(config: Arc<Config>) -> Result<()> {
+        // Create a new L1 RPC provider with the configured signer.
+        let rpc = Provider::<Http>::try_from(config.l1.node_url.as_str())?
+            .with_signer(config.get_configured_signer().await?);
+
+        // Construct the core.
+        let core = Kernel::new(rpc, config.clone());
+
+        // Bind the core to the RPC server.
+        let server_handle = AgglayerImpl::new(core).start(config).await?;
+
+        server_handle.stopped().await;
+
+        Ok(())
+    }
+}
