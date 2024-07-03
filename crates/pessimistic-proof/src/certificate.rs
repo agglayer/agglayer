72f8505534970229f312f2f276192ce4569aec9b
Monir Hadji <hadji.szs@gmail.com>
2024-07-03 14:55:54 +0400

    chore: add more doc around the certificate

--- crates/pessimistic-proof/src/certificate.rs
+++ crates/pessimistic-proof/src/certificate.rs
@@ -3,17 +3,26 @@ use serde::{Deserialize, Serialize};
 use crate::{bridge_exit::NetworkId, keccak::Digest, BridgeExit};
 
 /// Represents the data submitted by the CDKs to the AggLayer.
+///
+/// The bridge exits plus the imported bridge exits define
+/// the state transition, resp. the amount that goes out and the amount that comes in.
+///
+/// The bridge exits refer to the [`BridgeExit`]  emitted by
+/// the origin network of the [`Certificate`].
+///
+/// The imported bridge exits refer to the [`BridgeExit`] received and imported
+/// by the origin network of the [`Certificate`].
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Certificate {
-    /// Origin network which emitted this certificate
+    /// The origin network which emitted this certificate.
     pub origin_network: NetworkId,
-    /// Initial local exit root
+    /// The initial local exit root.
     pub prev_local_exit_root: Digest,
-    /// Set of bridge exits
+    /// The set of bridge exits emitted by the origin network.
     pub bridge_exits: Vec<BridgeExit>,
-    /// Set of imported bridge exits
+    /// The set of imported bridge exits for which the origin network is the target.
     pub imported_bridge_exits: Vec<BridgeExit>,
-    /// Signature that commits to the state transition
+    /// The signature that commits to the state transition.
     pub signature: (),
 }
 
