c64ba18b56b9362cee976f28fec50be1355e5485
Brendan Farmer <brendanfarmer@brendans-air-2.lan>
2024-07-10 20:18:57 -0400

    adding required public inputs to PP

--- crates/pessimistic-proof-program/src/main.rs
+++ crates/pessimistic-proof-program/src/main.rs
@@ -10,5 +10,23 @@ pub fn main() {
 
     let new_roots = generate_leaf_proof(initial_state, &batch_header).unwrap();
 
+    if let Some(imported_roots) = &batch_header.imported_lers_root {
+        sp1_zkvm::io::commit(imported_roots);
+    }else {
+        sp1_zkvm::io::commit(None);
+    }
+
+    if let Some(imported_global_root) = &batch_header.imported_global_exit_root {
+        sp1_zkvm::io::commit(imported_global_root);
+    }else {
+        sp1_zkvm::io::commit(None);
+    }
+
+    if let Some(imported_exits_root) = &batch_header.imported_exits_root {
+        sp1_zkvm::io::commit(imported_exits_root);
+    }else {
+        sp1_zkvm::io::commit(None);
+    }
+
     sp1_zkvm::io::commit(&new_roots);
 }
