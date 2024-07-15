dd5c49f212466ef0ebea17d963e1f47e46534286
Simon Paitrault <spaitrault@polygon.technology>
2024-07-15 12:05:29 +0200

    build: reduce coverage time
    Signed-off-by: Simon Paitrault <spaitrault@polygon.technology>

--- crates/agglayer-aggregator-notifier/src/tests/mod.rs
+++ crates/agglayer-aggregator-notifier/src/tests/mod.rs
@@ -8,31 +8,10 @@ mod sp1 {
     use super::*;
 
     #[tokio::test]
-    #[cfg_attr(not(feature = "sp1-network"), ignore)]
-    async fn aggregator_notifier_for_network() {
-        let config = ProverConfig::SP1Network {};
-        let notifier = AggregatorNotifier::<()>::try_from(config);
-        assert!(notifier.is_ok());
-
-        let notifier = notifier.unwrap();
-
-        let _result = notifier.pack(1, vec![]).unwrap().await;
-    }
-
-    #[tokio::test]
-    async fn aggregator_notifier_for_local() {
-        let config = ProverConfig::SP1Local {};
-        let notifier = AggregatorNotifier::<()>::try_from(config);
-        assert!(notifier.is_ok());
-
-        let notifier = notifier.unwrap();
-
-        let _result = notifier.pack(1, vec![]).unwrap().await;
-    }
-
-    #[tokio::test]
-    async fn aggregator_notifier_for_mock() {
-        let config = ProverConfig::SP1Mock {};
+    #[rstest::rstest]
+    #[case(ProverConfig::SP1Local {})]
+    #[case(ProverConfig::SP1Mock {})]
+    async fn aggregator_notifier_can_be_implemented(#[case] config: ProverConfig) {
         let notifier = AggregatorNotifier::<()>::try_from(config);
         assert!(notifier.is_ok());
 
