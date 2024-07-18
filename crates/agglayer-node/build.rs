d05a899967218d50ce29b62aea61a5f184a1167b
Simon Paitrault <spaitrault@polygon.technology>
2024-07-18 10:48:01 +0200

    chore: refactor logs for error rpc
    Signed-off-by: Simon Paitrault <spaitrault@polygon.technology>

---
+++ crates/agglayer-node/build.rs
@@ -0,0 +1,34 @@
+use std::path::Path;
+
+fn main() {
+    let contract_dir = Path::new(env!("CARGO_MANIFEST_DIR")).join("src/contracts");
+
+    build_contract(
+        "polygonzkevm.json",
+        "polygon_zk_evm.rs",
+        "PolygonZkEvm",
+        &contract_dir,
+    );
+    build_contract(
+        "polygonrollupmanager.json",
+        "polygon_rollup_manager.rs",
+        "PolygonRollupManager",
+        &contract_dir,
+    );
+}
+
+fn build_contract(source: &str, destination: &str, struct_name: &str, contract_dir: &Path) {
+    let abi_source = contract_dir.join(source);
+    let out_file = contract_dir.join(destination);
+
+    if out_file.exists() {
+        std::fs::remove_file(&out_file).unwrap();
+    }
+
+    ethers_contract::Abigen::new(struct_name, abi_source.display().to_string())
+        .unwrap()
+        .generate()
+        .unwrap()
+        .write_to_file(out_file)
+        .unwrap();
+}
--- crates/agglayer-node/src/contracts/mod.rs
+++ crates/agglayer-node/src/contracts/mod.rs
@@ -1,16 +1,8 @@
 //! Agglayer smart-contract bindings.
 
-pub(crate) mod polygon_rollup_manager {
-    use ethers::contract::abigen;
-
-    abigen!(
-        PolygonRollupManager,
-        "./src/contracts/polygonrollupmanager.json",
-    );
-}
-
-pub(crate) mod polygon_zk_evm {
-    use ethers::contract::abigen;
-
-    abigen!(PolygonZkEvm, "./src/contracts/polygonzkevm.json",);
-}
+#[rustfmt::skip]
+#[allow(warnings)]
+pub(crate) mod polygon_rollup_manager;
+#[rustfmt::skip]
+#[allow(warnings)]
+pub(crate) mod polygon_zk_evm;
