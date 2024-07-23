0969ccf36ab7efbbde7046b1a6a4286f24c3f0b5
Simon Paitrault <spaitrault@polygon.technology>
2024-07-23 10:57:55 +0200

    feat: adding nonce-manager
    Signed-off-by: Simon Paitrault <spaitrault@polygon.technology>

--- crates/agglayer-node/src/kernel/mod.rs
+++ crates/agglayer-node/src/kernel/mod.rs
@@ -4,7 +4,7 @@ use std::sync::Arc;
 use agglayer_config::Config;
 use ethers::prelude::*;
 use thiserror::Error;
-use tracing::{info, instrument};
+use tracing::{info, instrument, warn};
 
 use crate::{
     contracts::{
@@ -329,11 +329,22 @@ where
         &self,
         signed_tx: &SignedTx,
     ) -> Result<TransactionReceipt, SettlementError<RpcProvider>> {
-        let hash = signed_tx.hash().to_string();
+        let hex_hash = signed_tx.hash();
+        let hash = format!("{:?}", hex_hash);
+
         let f = self
             .build_verify_batches_trusted_aggregator_call(signed_tx)
             .await
             .map_err(SettlementError::ContractError)?;
+
+        if let Ok(Some(tx)) = self.check_tx_status(hex_hash).await {
+            warn!(hash, "Transaction already settled: {:?}", tx);
+        }
+
+        info!(
+            "Calling `verify_batches_trusted_aggregator` with {:?} nonce",
+            f.tx.nonce()
+        );
 
         let tx = f
             .send()
