5e357be046da3a4914eb617e1fbbfcc27ee983b4
Simon Paitrault <spaitrault@polygon.technology>
2024-08-27 15:17:58 +0200

    feat: adding physical storage
    Signed-off-by: Simon Paitrault <spaitrault@polygon.technology>

---
+++ crates/agglayer-storage/src/storage/iterators/mod.rs
@@ -0,0 +1,57 @@
+use crate::{
+    columns::{Codec as _, ColumnSchema},
+    error::Error,
+};
+
+enum IteratorStatus {
+    Initialized,
+    Progressing,
+    Done,
+}
+
+pub struct KeysIterator<'a, C: ColumnSchema> {
+    iter: rocksdb::DBRawIterator<'a>,
+    status: IteratorStatus,
+    direction: rocksdb::Direction,
+    _phantom: std::marker::PhantomData<C>,
+}
+
+impl<'a, C: ColumnSchema> KeysIterator<'a, C> {
+    pub(crate) fn new(iter: rocksdb::DBRawIterator<'a>, direction: rocksdb::Direction) -> Self {
+        Self {
+            iter,
+            direction,
+            status: IteratorStatus::Initialized,
+            _phantom: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<'a, C: ColumnSchema> Iterator for KeysIterator<'a, C> {
+    type Item = Result<C::Key, Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.status {
+            IteratorStatus::Done => {
+                return None;
+            }
+            IteratorStatus::Initialized => {
+                self.status = IteratorStatus::Progressing;
+            }
+
+            IteratorStatus::Progressing => match self.direction {
+                rocksdb::Direction::Forward => self.iter.next(),
+                rocksdb::Direction::Reverse => self.iter.prev(),
+            },
+        }
+
+        // If the iterator is invalid, return None
+        if !self.iter.valid() {
+            self.status = IteratorStatus::Done;
+
+            return None;
+        }
+
+        self.iter.key().map(C::Key::decode)
+    }
+}
---
+++ crates/agglayer-storage/src/tests.rs
@@ -0,0 +1,43 @@
+use std::{
+    env::temp_dir,
+    fs::create_dir_all,
+    path::PathBuf,
+    time::{SystemTime, UNIX_EPOCH},
+};
+
+use rand::Rng as _;
+
+pub struct TempDBDir {
+    pub(crate) path: PathBuf,
+}
+
+impl TempDBDir {
+    #[allow(dead_code)]
+    pub fn new() -> Self {
+        let mut path = temp_dir();
+
+        let folder_name = std::thread::current().name().unwrap().replace("::", "_");
+        let time = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Failed to get time since epoch");
+
+        let mut rng = rand::thread_rng();
+
+        path.push(format!(
+            "{}/{}_{}",
+            folder_name,
+            time.as_nanos(),
+            rng.gen::<u64>()
+        ));
+
+        create_dir_all(path.clone()).expect("Failed to create temp dir");
+
+        Self { path }
+    }
+}
+
+impl Drop for TempDBDir {
+    fn drop(&mut self) {
+        std::fs::remove_dir_all(&self.path).unwrap();
+    }
+}
