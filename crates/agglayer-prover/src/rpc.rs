23dd2a3b18baa3f6dfe0eca7defc8b1ff1435772
Simon Paitrault <spaitrault@polygon.technology>
2024-09-11 10:16:53 +0200

    feat: adding metrics
    Signed-off-by: Simon Paitrault <spaitrault@polygon.technology>

--- crates/agglayer-prover/src/rpc.rs
+++ crates/agglayer-prover/src/rpc.rs
@@ -1,4 +1,5 @@
 use agglayer_prover_types::v1::proof_generation_service_server::ProofGenerationService;
+use agglayer_telemetry::prover::{PROVING_REQUEST_RECV, PROVING_REQUEST_SUCCEED};
 use tracing::debug;
 
 #[derive(Default)]
@@ -11,9 +12,13 @@ impl ProofGenerationService for ProverRPC {
         request: tonic::Request<agglayer_prover_types::v1::ProofGenerationRequest>,
     ) -> Result<tonic::Response<agglayer_prover_types::v1::ProofGenerationResponse>, tonic::Status>
     {
+        let metrics_attrs = &[];
+        PROVING_REQUEST_RECV.add(1, metrics_attrs);
+
         debug!("Got a request from {:?}", request.remote_addr());
         let reply = agglayer_prover_types::v1::ProofGenerationResponse {};
 
+        PROVING_REQUEST_SUCCEED.add(1, metrics_attrs);
         Ok(tonic::Response::new(reply))
     }
 }
--- crates/agglayer-telemetry/src/constant.rs
+++ crates/agglayer-telemetry/src/constant.rs
@@ -1,2 +1,3 @@
 pub(crate) const AGGLAYER_RPC_OTEL_SCOPE_NAME: &str = "rpc";
 pub(crate) const AGGLAYER_KERNEL_OTEL_SCOPE_NAME: &str = "kernel";
+pub(crate) const AGGLAYER_PROVER_RPC_OTEL_SCOPE_NAME: &str = "agglayer_prover_rpc";
