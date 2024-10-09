19f97cb1d48e4689d61ec66093f9ab7839263cd1
Monir Hadji <hadji.szs@gmail.com>
2024-10-09 11:33:58 +0400

    chore: add sanity check

--- crates/pessimistic-proof-test-suite/tests/cycle-tracker.rs
+++ crates/pessimistic-proof-test-suite/tests/cycle-tracker.rs
@@ -4,6 +4,11 @@ use tracing::{debug, info};
 
 #[rstest::rstest]
 #[case::empty(Forest::new([]), std::iter::empty())]
+fn sanity_check(#[case] state: Forest, #[case] bridge_exits: impl Iterator<Item = BridgeExit>) {
+    cycles_on_sample_inputs_inner(state, bridge_exits);
+}
+
+#[rstest::rstest]
 #[case::s01_n000(data::sample_state_01(), std::iter::empty())]
 #[case::s01_n001(data::sample_state_01(), data::sample_bridge_exits_01().take(1))]
 #[case::s01_n002(data::sample_state_01(), data::sample_bridge_exits_01().take(2))]
@@ -12,8 +17,15 @@ use tracing::{debug, info};
 #[case::s01_full(data::sample_state_01(), data::sample_bridge_exits_01())]
 #[ignore = "Too expensive to run by default"]
 fn cycles_on_sample_inputs(
-    #[case] mut state: Forest,
+    #[case] state: Forest,
     #[case] bridge_exits: impl Iterator<Item = BridgeExit>,
+) {
+    cycles_on_sample_inputs_inner(state, bridge_exits);
+}
+
+fn cycles_on_sample_inputs_inner(
+    mut state: Forest,
+    bridge_exits: impl Iterator<Item = BridgeExit>,
 ) {
     sp1_sdk::utils::setup_logger();
 
