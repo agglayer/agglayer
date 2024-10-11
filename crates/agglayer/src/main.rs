78d60ae8ab25e86e4475268af09c7766bd201b63
Simon Paitrault <spaitrault@polygon.technology>
2024-10-11 10:49:08 +0200

    chore: adding prover config generator and remove useless doctests
    Signed-off-by: Simon Paitrault <spaitrault@polygon.technology>

--- crates/agglayer/src/main.rs
+++ crates/agglayer/src/main.rs
@@ -11,7 +11,15 @@ fn main() -> anyhow::Result<()> {
     match cli.cmd {
         cli::Commands::Run { cfg } => agglayer_node::main(cfg)?,
         cli::Commands::Prover { cfg } => agglayer_prover::main(cfg)?,
-        cli::Commands::Config { base_dir } => println!(
+        cli::Commands::Config { prover: true, .. } => println!(
+            "{}",
+            toml::to_string(&agglayer_config::prover::ProverConfig::default()).unwrap()
+        ),
+
+        cli::Commands::Config {
+            base_dir,
+            prover: false,
+        } => println!(
             "{}",
             toml::to_string(&agglayer_config::Config::new(&base_dir)).unwrap()
         ),
