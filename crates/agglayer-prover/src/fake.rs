6454fbf26554b21a140c946fa922495deba8108b
Simon Paitrault <spaitrault@polygon.technology>
2024-10-15 14:59:14 +0200

    refactor: adding l1_info_tree_leaf_count
    Signed-off-by: Simon Paitrault <spaitrault@polygon.technology>

--- crates/agglayer-prover/src/fake.rs
+++ crates/agglayer-prover/src/fake.rs
@@ -1,16 +1,42 @@
 use std::net::SocketAddr;
+use std::sync::Arc;
+use std::time::Duration;
 
 use agglayer_prover_types::v1::proof_generation_service_server::{
     ProofGenerationService, ProofGenerationServiceServer,
 };
-use agglayer_types::Proof;
+use agglayer_prover_types::Error;
+use agglayer_types::Keccak256Hasher;
 use bincode::Options;
+use pessimistic_proof::multi_batch_header::MultiBatchHeader;
+use pessimistic_proof::LocalNetworkState;
+use sp1_sdk::CpuProver;
+use sp1_sdk::Prover;
+use sp1_sdk::SP1Context;
+use sp1_sdk::SP1ProofKind;
 use tonic::transport::Server;
 use tracing::error;
 use tracing::info;
+use tracing::warn;
 
-#[derive(Default)]
-pub struct FakeProver {}
+use crate::executor::Request;
+use crate::executor::ELF;
+
+pub struct FakeProver {
+    prover: Arc<CpuProver>,
+    proving_key: sp1_sdk::SP1ProvingKey,
+}
+
+impl Default for FakeProver {
+    fn default() -> Self {
+        let prover = CpuProver::new();
+        let (proving_key, _) = prover.setup(ELF);
+        Self {
+            proving_key,
+            prover: Arc::new(prover),
+        }
+    }
+}
 
 impl FakeProver {
     pub async fn spawn_at(
@@ -62,15 +88,53 @@ impl FakeProver {
 impl ProofGenerationService for FakeProver {
     async fn generate_proof(
         &self,
-        _request: tonic::Request<agglayer_prover_types::v1::ProofGenerationRequest>,
+        request: tonic::Request<agglayer_prover_types::v1::ProofGenerationRequest>,
     ) -> Result<tonic::Response<agglayer_prover_types::v1::ProofGenerationResponse>, tonic::Status>
     {
-        Ok(tonic::Response::new(
-            agglayer_prover_types::v1::ProofGenerationResponse {
-                proof: agglayer_prover_types::default_bincode_options()
-                    .serialize(&Proof::new_for_test())
-                    .unwrap(),
-            },
-        ))
+        let request = request.into_inner();
+        let initial_state: LocalNetworkState = agglayer_prover_types::default_bincode_options()
+            .deserialize(&request.initial_state)
+            .map_err(|_| tonic::Status::invalid_argument("Unable to deserialize initial state"))?;
+        let batch_header: MultiBatchHeader<Keccak256Hasher> =
+            agglayer_prover_types::default_bincode_options()
+                .deserialize(&request.batch_header)
+                .map_err(|_| {
+                    tonic::Status::invalid_argument("Unable to deserialize batch header")
+                })?;
+        let request = Request {
+            initial_state,
+            batch_header,
+        };
+        let stdin = request.into();
+
+        let proof_opts = sp1_sdk::provers::ProofOpts {
+            timeout: Some(Duration::from_secs(60)),
+            ..Default::default()
+        };
+        let context = SP1Context::default();
+
+        let result = self
+            .prover
+            .prove(
+                &self.proving_key,
+                stdin,
+                proof_opts,
+                context,
+                SP1ProofKind::Core,
+            )
+            .map_err(|error| Error::ProverFailed(error.to_string()));
+        match result {
+            Ok(proof) => Ok(tonic::Response::new(
+                agglayer_prover_types::v1::ProofGenerationResponse {
+                    proof: agglayer_prover_types::default_bincode_options()
+                        .serialize(&proof)
+                        .unwrap(),
+                },
+            )),
+            Err(error) => {
+                warn!("FakeProver error: {}", error);
+                Err(tonic::Status::invalid_argument(error.to_string()))
+            }
+        }
     }
 }
