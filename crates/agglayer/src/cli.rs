5129b6a2f7ee8760428dfb4bcc64616349409d3d
Simon Paitrault <spaitrault@polygon.technology>
2024-10-17 19:05:28 +0200

    chore: update after review
    Signed-off-by: Simon Paitrault <spaitrault@polygon.technology>

--- crates/agglayer/src/cli.rs
+++ crates/agglayer/src/cli.rs
@@ -27,11 +27,9 @@ pub(crate) enum Commands {
             env = "CONFIG_PATH"
         )]
         base_dir: PathBuf,
-
-        /// Defines if the command is for generating a prover configuration.
-        #[arg(long)]
-        prover: bool,
     },
+
+    ProverConfig,
 
     Prover {
         /// The path to the configuration file.
