5f48d295bc402ceaef021de01e2e4fee1fadf63f
wborgeaud <williamborgeaud@gmail.com>
2024-10-31 08:54:51 +0100

    feat: add aggregation proof

---
+++ crates/pp-aggregation-combine-program/src/main.rs
@@ -0,0 +1,36 @@
+#![no_main]
+sp1_zkvm::entrypoint!(main);
+
+use bincode::Options;
+use pessimistic_proof::aggregation::combine::combine_proofs;
+use pessimistic_proof::aggregation::wrap::AggregationProofOutput;
+use sha2::Digest;
+use sha2::Sha256;
+
+pub fn main() {
+    // Read the verification keys.
+    let vkey = sp1_zkvm::io::read::<[u32; 8]>();
+
+    // Read the public values.
+    let public_values0 = sp1_zkvm::io::read::<Vec<u8>>();
+    let public_values1 = sp1_zkvm::io::read::<Vec<u8>>();
+
+    // Verify the first proof.
+    let public_values_digest0 = Sha256::digest(&public_values0);
+    sp1_zkvm::lib::verify::verify_sp1_proof(&vkey, &public_values_digest0.into());
+
+    // Verify the second proof.
+    let public_values_digest1 = Sha256::digest(&public_values1);
+    sp1_zkvm::lib::verify::verify_sp1_proof(&vkey, &public_values_digest1.into());
+
+    let wrapped_proof0: AggregationProofOutput = bincode::DefaultOptions::new()
+        .deserialize(&public_values0)
+        .expect("Failed to deserialize");
+    let wrapped_proof1: AggregationProofOutput = bincode::DefaultOptions::new()
+        .deserialize(&public_values1)
+        .expect("Failed to deserialize");
+
+    let combined_proof_output = combine_proofs(wrapped_proof0, wrapped_proof1);
+
+    sp1_zkvm::io::commit(&combined_proof_output);
+}
--- crates/pp-aggregation-wrap-program/src/main.rs
+++ crates/pp-aggregation-wrap-program/src/main.rs
@@ -2,13 +2,13 @@
 sp1_zkvm::entrypoint!(main);
 
 use bincode::Options;
-use pessimistic_proof::aggregation::wrap_proof;
+use pessimistic_proof::aggregation::wrap::wrap_proof;
 use pessimistic_proof::PessimisticProofOutput;
 use sha2::Digest;
 use sha2::Sha256;
 
 pub fn main() {
-    // Read the verification keys.
+    // Read the verification key.
     let vkey = sp1_zkvm::io::read::<[u32; 8]>();
 
     // Read the public values.
@@ -18,24 +18,24 @@ pub fn main() {
     let public_values_digest = Sha256::digest(&public_values);
     sp1_zkvm::lib::verify::verify_sp1_proof(&vkey, &public_values_digest.into());
 
-    let proof_output: PessimisticProofOutput = bincode::DefaultOptions::new()
+    let pp_output: PessimisticProofOutput = bincode::DefaultOptions::new()
         .deserialize(&public_values)
         .expect("Failed to deserialize");
 
-    let tmp_rer = sp1_zkvm::io::read::<_>();
-    let new_mer = sp1_zkvm::io::read::<_>();
-    let new_rer = sp1_zkvm::io::read::<_>();
-    let tmp_rer_proof = sp1_zkvm::io::read::<_>();
+    let tmp_arer = sp1_zkvm::io::read::<_>();
+    let selected_mer = sp1_zkvm::io::read::<_>();
+    let selected_rer = sp1_zkvm::io::read::<_>();
+    let tmp_arer_proof = sp1_zkvm::io::read::<_>();
     let imported_lers_witness = sp1_zkvm::io::read::<_>();
 
-    let stage1_agg_proof_output = wrap_proof(
-        proof_output,
-        tmp_rer,
-        new_mer,
-        new_rer,
-        tmp_rer_proof,
+    let wrapped_proof_output = wrap_proof(
+        pp_output,
+        tmp_arer,
+        selected_mer,
+        selected_rer,
+        tmp_arer_proof,
         imported_lers_witness,
     );
 
-    sp1_zkvm::io::commit(&stage1_agg_proof_output);
+    sp1_zkvm::io::commit(&wrapped_proof_output);
 }
