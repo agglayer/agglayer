d1dd592dcd5df6defbe0a458423a244043e7ee71
Marko Atanasievski <markoatana@gmail.com>
2024-12-20 18:07:14 +0100

    fix: use git commit timestamp

--- crates/agglayer/build.rs
+++ crates/agglayer/build.rs
@@ -1,9 +1,13 @@
-use vergen_git2::{BuildBuilder, Emitter, Git2Builder};
+use vergen_git2::{Emitter, Git2Builder};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     Emitter::new()
-        .add_instructions(&BuildBuilder::default().build_timestamp(true).build()?)?
-        .add_instructions(&Git2Builder::default().describe(true, true, None).build()?)?
+        .add_instructions(
+            &Git2Builder::default()
+                .describe(true, true, None)
+                .commit_timestamp(true)
+                .build()?,
+        )?
         .emit()?;
     Ok(())
 }
