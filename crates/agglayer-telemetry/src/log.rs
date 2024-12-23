b9ece4c3527408973112ede5cbd4557745a9b6f1
Marko Atanasievski <matanasievski@polygon.technology>
2024-12-23 16:10:10 +0100

    fix: nits

--- crates/agglayer-telemetry/src/log.rs
+++ crates/agglayer-telemetry/src/log.rs
@@ -9,7 +9,6 @@ use opentelemetry_sdk::{propagation::TraceContextPropagator, trace::Sampler, Res
 use tracing_subscriber::{prelude::*, util::SubscriberInitExt, EnvFilter};
 
 pub fn setup_tracing(config: &agglayer_config::Log, version: &str) -> anyhow::Result<()> {
-
     let writer = config.outputs.first().cloned().unwrap_or_default();
 
     let mut layers = Vec::new();
@@ -56,39 +55,41 @@ pub fn setup_tracing(config: &agglayer_config::Log, version: &str) -> anyhow::Re
                     },
                 );
 
+            let span_limits_default = SpanLimits::default();
+
             let trace_provider = opentelemetry_sdk::trace::Builder::default()
                 .with_sampler(Sampler::AlwaysOn)
                 .with_max_events_per_span(match std::env::var("OTLP_MAX_EVENTS_PER_SPAN") {
                     Ok(v) => v
                         .parse::<u32>()
-                        .unwrap_or(SpanLimits::default().max_events_per_span),
-                    _ => SpanLimits::default().max_events_per_span,
+                        .unwrap_or(span_limits_default.max_events_per_span),
+                    _ => span_limits_default.max_events_per_span,
                 })
                 .with_max_attributes_per_span(match std::env::var("OTLP_MAX_ATTRIBUTES_PER_SPAN") {
                     Ok(v) => v
                         .parse::<u32>()
-                        .unwrap_or(SpanLimits::default().max_attributes_per_span),
-                    _ => SpanLimits::default().max_attributes_per_span,
+                        .unwrap_or(span_limits_default.max_attributes_per_span),
+                    _ => span_limits_default.max_attributes_per_span,
                 })
                 .with_max_links_per_span(match std::env::var("OTLP_MAX_LINK_PER_SPAN") {
                     Ok(v) => v
                         .parse::<u32>()
-                        .unwrap_or(SpanLimits::default().max_links_per_span),
-                    _ => SpanLimits::default().max_links_per_span,
+                        .unwrap_or(span_limits_default.max_links_per_span),
+                    _ => span_limits_default.max_links_per_span,
                 })
                 .with_max_attributes_per_event(
                     match std::env::var("OTLP_MAX_ATTRIBUTES_PER_EVENT") {
                         Ok(v) => v
                             .parse::<u32>()
-                            .unwrap_or(SpanLimits::default().max_attributes_per_event),
-                        _ => SpanLimits::default().max_attributes_per_event,
+                            .unwrap_or(span_limits_default.max_attributes_per_event),
+                        _ => span_limits_default.max_attributes_per_event,
                     },
                 )
                 .with_max_attributes_per_link(match std::env::var("OTLP_MAX_ATTRIBUTES_PER_LINK") {
                     Ok(v) => v
                         .parse::<u32>()
-                        .unwrap_or(SpanLimits::default().max_attributes_per_link),
-                    _ => SpanLimits::default().max_attributes_per_link,
+                        .unwrap_or(span_limits_default.max_attributes_per_link),
+                    _ => span_limits_default.max_attributes_per_link,
                 })
                 .with_resource(Resource::new(resources))
                 .with_span_processor(
@@ -113,9 +114,7 @@ pub fn setup_tracing(config: &agglayer_config::Log, version: &str) -> anyhow::Re
 
             global::set_text_map_propagator(TraceContextPropagator::new());
         } else {
-            anyhow::bail!(
-                "Otlp tracing requires both otlp agent url and otlp service provided"
-            );
+            anyhow::bail!("Otlp tracing requires both otlp agent url and otlp service provided");
         }
     }
 
@@ -138,7 +137,9 @@ pub fn setup_tracing(config: &agglayer_config::Log, version: &str) -> anyhow::Re
     _ = tracing_subscriber::Registry::default()
         .with(layers)
         .try_init();
-    
+
+    tracing::info!("Tracing initialized with config: {config:?}");
+
     Ok(())
 }
 
