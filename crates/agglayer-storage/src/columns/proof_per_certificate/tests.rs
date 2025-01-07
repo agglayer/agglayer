fe876ab10821d2c039d83b3c21b0770f111169df
Simon Paitrault <spaitrault@polygon.technology>
2025-01-07 20:52:55 +0100

    feat: adding regression tests on sp1 proof
    Signed-off-by: Simon Paitrault <spaitrault@polygon.technology>

--- crates/agglayer-storage/src/columns/proof_per_certificate/tests.rs
+++ crates/agglayer-storage/src/columns/proof_per_certificate/tests.rs
@@ -1,3 +1,11 @@
+use agglayer_types::{Digest, LocalNetworkStateData};
+use insta::assert_snapshot;
+use pessimistic_proof::{keccak::keccak256_combine, LocalNetworkState, PessimisticProofOutput};
+use pessimistic_proof_test_suite::{forest::Forest, sample_data::USDC, PESSIMISTIC_PROOF_ELF};
+use sp1_sdk::{
+    provers::ProofOpts, MockProver, Prover, SP1Context, SP1ProofWithPublicValues, SP1Stdin,
+};
+
 use crate::columns::{
     proof_per_certificate::{CertificateId, Proof},
     Codec as _,
@@ -31,4 +39,19 @@ fn can_parse_value() {
     let expected_value = Proof::decode(&encoded[..]).expect("Unable to decode value");
 
     assert!(matches!(expected_value, Proof::SP1(_)));
+}
+
+#[test]
+fn non_regression_proof_encoding() {
+    let path = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("./src/columns/proof_per_certificate/fixtures/non_regression_proof_encoding.proof");
+
+    let proof = SP1ProofWithPublicValues::load(path).unwrap();
+
+    assert_snapshot!("proof hex format", hex::encode(proof.bytes()));
+
+    assert_snapshot!(
+        "proof public input hex format",
+        hex::encode(proof.public_values.as_slice())
+    );
 }
