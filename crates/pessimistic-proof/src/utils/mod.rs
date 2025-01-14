51b7cd7c717b3111e6b51273fffadad1303bb439
Simon Paitrault <spaitrault@polygon.technology>
2025-01-14 15:29:51 +0100

    docs: adding doc to hashable
    Signed-off-by: Simon Paitrault <spaitrault@polygon.technology>

--- crates/pessimistic-proof/src/utils/mod.rs
+++ crates/pessimistic-proof/src/utils/mod.rs
@@ -3,6 +3,8 @@ use pessimistic_proof_core::keccak::digest::Digest;
 pub mod empty_hash;
 pub mod smt;
 
+/// Trait for objects that can be hashed.
 pub trait Hashable {
+    /// Hashes the object to a [`Digest`].
     fn hash(&self) -> Digest;
 }
