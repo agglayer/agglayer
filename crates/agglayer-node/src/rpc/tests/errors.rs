a4c50e0f38f493fb1911f2e53f577bd102069f30
Ekleog-Polygon <lgaspard@polygon.technology>
2025-01-31 10:19:18 +0100

    feat: confirm certificates’ claimed emitter is allowed to emit (#565)

--- crates/agglayer-node/src/rpc/tests/errors.rs
+++ crates/agglayer-node/src/rpc/tests/errors.rs
@@ -4,6 +4,7 @@ use std::time::Duration;
 
 use agglayer_contracts::polygon_rollup_manager::PolygonRollupManagerErrors;
 use agglayer_types::Digest;
+use alloy::{primitives::SignatureError as AlloySignatureError, signers::k256};
 use ethers::{
     providers::ProviderError,
     types::{Bytes, SignatureError as EthSignatureError, H160, H256},
@@ -30,17 +31,23 @@ type WallClockLimitedInfo = <component::SendTx as Component>::LimitedInfo;
 #[case("rollup_not_reg", SendTxError::RollupNotRegistered { rollup_id: 1337 })]
 #[case(
     "sig_invalid_len",
-    SendTxError::SignatureError(SignatureError::CouldNotRecoverSigner(
+    SendTxError::SignatureError(SignatureError::CouldNotRecoverTxSigner(
         EthSignatureError::InvalidLength(42)
     ))
 )]
-#[case("sig_verif", SendTxError::SignatureError(SignatureError::CouldNotRecoverSigner(
+#[case("sig_verif", SendTxError::SignatureError(SignatureError::CouldNotRecoverTxSigner(
     EthSignatureError::VerificationError(H160([0x11; 20]), H160([0x22; 20]))
 )))]
 #[case(
     "sig_recov",
-    SendTxError::SignatureError(SignatureError::CouldNotRecoverSigner(
+    SendTxError::SignatureError(SignatureError::CouldNotRecoverTxSigner(
         EthSignatureError::RecoveryError
+    ))
+)]
+#[case(
+    "cert_sig",
+    SendTxError::SignatureError(SignatureError::CouldNotRecoverCertSigner(
+        AlloySignatureError::K256(k256::ecdsa::Error::new())
     ))
 )]
 #[case(
