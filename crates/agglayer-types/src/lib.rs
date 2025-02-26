82bb631ab7f5e62f7195dd46b96878c0f1563276
Ekleog-Polygon <lgaspard@polygon.technology>
2025-02-26 15:40:59 +0100

    review comments

--- crates/agglayer-types/src/lib.rs
+++ crates/agglayer-types/src/lib.rs
@@ -189,18 +189,23 @@ impl std::fmt::Display for GenerationType {
 pub enum CertificateStatus {
     /// Received certificate from the network, nothing checked yet.
     ///
-    /// Certificate will stay in this state until the L1InfoRoot is finalized.
-    /// It will then be queued to be sent to agglayer-prover for PP generation.
+    /// Certificate will stay in this state until rate-limiting is lifted or an
+    /// epoch-change event is triggered. A pending certificate can then be
+    /// processed by the agglayer to be proven, or it could end up in error.
     Pending,
 
     /// Pessimistic proof has been generated for the certificate and stored in
     /// the rocksdb in the agglayer node.
     Proven,
 
-    /// Certificate proof has been acknowledged by L1, but the contract call has
-    /// not returned its verification feedback yet.
+    /// Settlement of the certificate's proof has already been started on L1
+    /// (and acknowledged by its RPC) by issuing a contract call to the
+    /// RollupManager, but the associated transaction has not yet seen
+    /// enough confirmations.
     ///
-    /// Waiting for L1 to execute the contract call.
+    /// The certificate can move from Candidate to Settled if the associated
+    /// transaction is accepted and the transaction receipt is a success. If the
+    /// transaction receipt fails, the certificate will end up in Error.
     Candidate,
 
     /// Hit some error while moving the certificate through the pipeline.
