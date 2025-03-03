8a76148f37b7469bb2f12756dd21e78c91c442a3
Ekleog-Polygon <lgaspard@polygon.technology>
2025-03-03 14:02:08 +0100

    feat: improve error type for grpc compat layer (#672)

--- crates/agglayer-grpc-types/src/compat/v1/bytes.rs
+++ crates/agglayer-grpc-types/src/compat/v1/bytes.rs
@@ -5,12 +5,9 @@ impl TryFrom<v1::FixedBytes20> for [u8; 20] {
     type Error = Error;
 
     fn try_from(value: v1::FixedBytes20) -> Result<Self, Self::Error> {
-        (&*value.value)
-            .try_into()
-            .map_err(|_| Error::WrongBytesLength {
-                expected: 20,
-                actual: value.value.len(),
-            })
+        (&*value.value).try_into().map_err(|_| {
+            Error::invalid_data(format!("expected 20 bytes, got {}", value.value.len()))
+        })
     }
 }
 
@@ -18,11 +15,8 @@ impl TryFrom<v1::FixedBytes32> for [u8; 32] {
     type Error = Error;
 
     fn try_from(value: v1::FixedBytes32) -> Result<Self, Self::Error> {
-        (&*value.value)
-            .try_into()
-            .map_err(|_| Error::WrongBytesLength {
-                expected: 32,
-                actual: value.value.len(),
-            })
+        (&*value.value).try_into().map_err(|_| {
+            Error::invalid_data(format!("expected 32 bytes, got {}", value.value.len()))
+        })
     }
 }
