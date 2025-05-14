d47fe3c352bac522a21015135684e3a328fae194
L Kuklinek <lkuklinek@polygon.technology>
2025-05-14 18:57:21 +0100

    chore: wip

--- crates/agglayer-storage/src/types/certificate/tests/mod.rs
+++ crates/agglayer-storage/src/types/certificate/tests/mod.rs
@@ -2,11 +2,14 @@ use agglayer_types::{
     aggchain_proof::{Proof, SP1StarkWithContext},
     U256,
 };
+use alloy_primitives::Address;
 use pessimistic_proof_test_suite::sample_data;
 use sp1_sdk::Prover;
 
 use super::*;
 use crate::columns::Codec;
+
+mod status;
 
 #[rstest::rstest]
 #[case(0.into(), [0, 0, 0, 0])]
@@ -247,4 +250,67 @@ fn bad_format() {
     }
 }
 
-mod status;
+fn trace() -> serde_reflection::Result<serde_reflection::Registry> {
+    // Take a snapshot of the internal structure of the encoding of Certificate
+    // to avoid accidental changes.
+    let tracer_config = serde_reflection::TracerConfig::default().is_human_readable(false);
+    let mut tracer = serde_reflection::Tracer::new(tracer_config);
+    let mut samples = serde_reflection::Samples::new();
+
+    #[derive(Clone, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq)]
+    pub enum ProofError {
+        InvalidPreviousPessimisticRoot {
+            declared: Digest,
+            computed_v2: Digest,
+            computed_v3: Digest,
+        },
+        /*
+        InvalidImportedBridgeExit {
+            source: Error,
+            global_index: GlobalIndex,
+        },
+        */
+        InvalidImportedExitsRoot {
+            declared: Digest,
+            computed: Digest,
+        },
+        InvalidNullifierPath,
+        InvalidBalancePath,
+        BalanceOverflowInBridgeExit,
+        BalanceUnderflowInBridgeExit,
+        CannotExitToSameNetwork,
+        InvalidMessageOriginNetwork,
+        //InvalidL1TokenInfo(TokenInfo),
+        //MissingTokenBalanceProof(TokenInfo),
+        //DuplicateTokenBalanceProof(TokenInfo),
+        InvalidSignature,
+        InconsistentSignedPayload,
+        InvalidSigner {
+            declared: Address,
+            recovered: Address,
+        },
+        //InvalidLocalExitTreeOperation(LocalExitTreeError),
+        Unknown(String),
+        HeightOverflow,
+    }
+
+    tracer.trace_value::<Address>(&mut samples, &Address::new([0xad; 20]))?;
+    tracer.trace_value::<Digest>(&mut samples, &Digest([0xd1; 32]))?;
+    insta::assert_debug_snapshot!(samples);
+    tracer.trace_simple_type::<agglayer_types::GenerationType>()?;
+    //tracer.trace_simple_type::<agglayer_types::CertificateStatus>().unwrap();
+    //tracer.trace_simple_type::<agglayer_types::CertificateStatusError>().unwrap();
+    tracer.trace_type::<ProofError>(&samples)?;
+    tracer.trace_simple_type::<pessimistic_proof::error::ProofVerificationError>()?;
+
+    //let registry = tracer.registry().unwrap();
+    tracer.registry()
+}
+
+#[test]
+fn structure_snapshot() {
+    match trace() {
+        Ok(registry) => insta::assert_debug_snapshot!(registry),
+        Err(err) => panic!("{err}: {}", err.explanation()),
+    }
+}
