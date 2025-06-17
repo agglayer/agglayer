cec99f417b52ac83eb9805ac1136bc5eac51c2cd
Ekleog-Polygon <lgaspard@polygon.technology>
2025-06-17 17:07:12 +0200

    review comments

--- crates/agglayer-certificate-orchestrator/src/network_task.rs
+++ crates/agglayer-certificate-orchestrator/src/network_task.rs
@@ -335,13 +335,13 @@ where
                         continue;
                     }
                     Some(NetworkTaskMessage::CertificateSettled { settled_certificate, .. }) => {
-                        self.at_capacity_for_epoch = true;
-                        self.latest_settled = Some(settled_certificate);
-                        *next_expected_height += 1;
                         debug!(%certificate_id, "Certification process completed");
                         let Some(new) = self.pending_state.take() else {
                             return Err(Error::InternalError(format!("Missing pending state needed upon settlement, current state: {}", self.local_state.get_roots().display_to_hex() )))
                         };
+                        self.at_capacity_for_epoch = true;
+                        self.latest_settled = Some(settled_certificate);
+                        *next_expected_height += 1;
                         debug!(
                             old_state = self.local_state.get_roots().display_to_hex(),
                             new_state = new.get_roots().display_to_hex(),
