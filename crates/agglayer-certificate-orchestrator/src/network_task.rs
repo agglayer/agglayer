38e54d4d5cc760d49b7208fbeb9484ac31584675
Ekleog-Polygon <lgaspard@polygon.technology>
2025-06-24 17:29:29 +0200

    fix tests and add todo for the failure we should handle

--- crates/agglayer-certificate-orchestrator/src/network_task.rs
+++ crates/agglayer-certificate-orchestrator/src/network_task.rs
@@ -369,6 +369,7 @@ where
                         settlement_submitted_notifier
                             .send(result)
                             .map_err(|_| Error::InternalError("Certificate notification channel closed".into()))?;
+                        fail::fail_point!("network_task::settlement_submitted");
                         continue;
                     }
                     Some(NetworkTaskMessage::CertificateWaitingForSettlement { settlement_tx_hash, settlement_complete_notifier, .. }) => {
