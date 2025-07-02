881544d18d50701a0f79182e2b8f661c74ff5994
L Kuklinek <lkuklinek@polygon.technology>
2025-07-02 08:55:44 +0100

    chore: change formatting to be more diff-able

--- crates/pessimistic-proof-test-suite/tests/vkey_selector.rs
+++ crates/pessimistic-proof-test-suite/tests/vkey_selector.rs
@@ -2,14 +2,16 @@
 fn vkey_snapshot() {
     let selector = hex::encode(pessimistic_proof::core::PESSIMISTIC_PROOF_PROGRAM_SELECTOR);
     let vkey = agglayer_prover::compute_program_vkey(pessimistic_proof::ELF);
-    let col1 = "SELECTOR";
-    let col2 = "VKEY";
 
-    let message = format!(
-        "# If this test fails, it means the PP vkey has changed.\n# When that happens, consider \
-         updating the selector by bumping the PP version.\n| {col1:10} | {col2:66} |\n| \
-         0x{selector} | {vkey} |\n",
-    );
+    let message: String = [
+        "# If this test fails, it means the PP vkey has changed.",
+        "# When that happens, consider updating the selector by bumping the PP version.",
+        &format!("| PP_VKEY          | {vkey} |"),
+        &format!("| PP_VKEY_SELECTOR | 0x{selector:64} |"),
+    ]
+    .into_iter()
+    .flat_map(|line| [line, "\n"])
+    .collect();
 
     insta::assert_snapshot!(message);
 }
