851b0c6607bf094381d70c2d59b1d87ea84eea5d
Leo Gaspard <lgaspard@polygon.technology>
2025-07-03 21:22:17 +0200

    feat: add a private gRPC port, and private networks that can only submit through it

---
+++ crates/agglayer-config/src/private_networks.rs
@@ -0,0 +1,46 @@
+use std::net::Ipv4Addr;
+
+use agglayer_types::NetworkId;
+use serde::{Deserialize, Deserializer, Serialize};
+use serde_with::serde_as;
+
+use crate::from_env_or_default;
+
+/// The local RPC server configuration.
+#[serde_as]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+#[serde(rename_all = "kebab-case")]
+pub struct PrivateNetworksConfig {
+    /// List of the private networks.
+    pub networks: Vec<NetworkId>,
+
+    /// The port for the local gRPC server for private networks.
+    /// Overridden by `AGGLAYER_PRIVATE_GRPC_PORT` environment variable.
+    #[serde(deserialize_with = "deserialize_grpc_port")]
+    pub grpc_port: u16,
+
+    /// The host for the local RPC server for private networks.
+    pub host: Ipv4Addr,
+}
+
+impl PrivateNetworksConfig {
+    /// Create a new `PrivateNetworksConfig` with default values.
+    #[cfg(feature = "testutils")]
+    pub fn for_tests(networks: Vec<NetworkId>) -> Self {
+        Self {
+            networks,
+            // Invalid host/port, to be overridden by the test context
+            grpc_port: u16::MAX,
+            host: Ipv4Addr::new(255, 255, 255, 255),
+        }
+    }
+}
+
+fn deserialize_grpc_port<'de, D>(deserializer: D) -> Result<u16, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let port = u16::deserialize(deserializer)?;
+
+    Ok(from_env_or_default("AGGLAYER_PRIVATE_GRPC_PORT", port))
+}
