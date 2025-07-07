b18799446e0ef7d042b42f04a5c5b913065f4a78
Ekleog-Polygon <lgaspard@polygon.technology>
2025-07-07 12:18:38 +0200

    feat: add a private gRPC port, and private networks that can only submit through it (#877)

---
+++ crates/agglayer-config/src/proxied_networks.rs
@@ -0,0 +1,46 @@
+use std::net::Ipv4Addr;
+
+use agglayer_types::NetworkId;
+use serde::{Deserialize, Deserializer, Serialize};
+use serde_with::serde_as;
+
+use crate::from_env_or_default;
+
+/// The local RPC server configuration.
+#[serde_as]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+#[serde(rename_all = "kebab-case")]
+pub struct ProxiedNetworksConfig {
+    /// List of the proxied networks.
+    pub networks: Vec<NetworkId>,
+
+    /// The port for the local gRPC server for proxied networks.
+    /// Overridden by `AGGLAYER_PROXIED_GRPC_PORT` environment variable.
+    #[serde(deserialize_with = "deserialize_grpc_port")]
+    pub grpc_port: u16,
+
+    /// The host for the local RPC server for proxied networks.
+    pub host: Ipv4Addr,
+}
+
+impl ProxiedNetworksConfig {
+    /// Create a new `ProxiedNetworksConfig` with default values.
+    #[cfg(feature = "testutils")]
+    pub fn for_tests(networks: Vec<NetworkId>) -> Self {
+        Self {
+            networks,
+            // Invalid host/port, to be overridden by the test context
+            grpc_port: u16::MAX,
+            host: Ipv4Addr::new(255, 255, 255, 255),
+        }
+    }
+}
+
+fn deserialize_grpc_port<'de, D>(deserializer: D) -> Result<u16, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let port = u16::deserialize(deserializer)?;
+
+    Ok(from_env_or_default("AGGLAYER_PROXIED_GRPC_PORT", port))
+}
--- crates/agglayer-jsonrpc-api/src/testutils.rs
+++ crates/agglayer-jsonrpc-api/src/testutils.rs
@@ -1,6 +1,6 @@
 use std::{
     future::IntoFuture,
-    net::{IpAddr, SocketAddr},
+    net::{IpAddr, Ipv4Addr, SocketAddr},
     sync::Arc,
 };
 
@@ -25,14 +25,16 @@ use tokio_util::sync::CancellationToken;
 
 use crate::{admin::AdminAgglayerImpl, kernel::Kernel, service::AgglayerService, AgglayerImpl};
 
+type MockAgglayerImpl = AgglayerImpl<
+    Provider<MockProvider>,
+    L1RpcClient<Provider<MockProvider>>,
+    PendingStore,
+    StateStore,
+    DebugStore,
+    Box<dyn 'static + Send + Sync + Fn(NetworkId) -> bool>,
+>;
 pub(crate) struct RawRpcContext {
-    pub(crate) rpc: AgglayerImpl<
-        Provider<MockProvider>,
-        L1RpcClient<Provider<MockProvider>>,
-        PendingStore,
-        StateStore,
-        DebugStore,
-    >,
+    pub(crate) rpc: MockAgglayerImpl,
     pub(crate) admin_rpc: AdminAgglayerImpl<PendingStore, StateStore, DebugStore>,
     pub(crate) config: Arc<Config>,
     pub(crate) state_store: Arc<StateStore>,
@@ -47,6 +49,7 @@ pub struct TestContext {
     pub pending_store: Arc<PendingStore>,
     pub client: jsonrpsee::http_client::HttpClient,
     pub admin_client: jsonrpsee::http_client::HttpClient,
+    pub proxied_grpc_client: Option<jsonrpsee::http_client::HttpClient>,
     pub config: Arc<Config>,
     pub certificate_receiver: tokio::sync::mpsc::Receiver<(NetworkId, Height, CertificateId)>,
 }
@@ -72,6 +75,10 @@ impl TestContext {
         let admin_url = format!("http://{}/", raw_rpc.config.admin_rpc_addr());
         let client = HttpClientBuilder::default().build(api_url).unwrap();
         let admin_client = HttpClientBuilder::default().build(admin_url).unwrap();
+        let proxied_grpc_client = raw_rpc.config.proxied_networks.as_ref().map(|pn| {
+            let proxied_url = format!("http://{}:{}", pn.host, pn.grpc_port);
+            HttpClientBuilder::default().build(proxied_url).unwrap()
+        });
 
         let listener_api = tokio::net::TcpListener::bind(raw_rpc.config.readrpc_addr())
             .await
@@ -95,6 +102,7 @@ impl TestContext {
             pending_store: raw_rpc.pending_store,
             client,
             admin_client,
+            proxied_grpc_client,
             config: raw_rpc.config,
             certificate_receiver: raw_rpc.certificate_receiver,
         }
@@ -125,6 +133,14 @@ impl TestContext {
         }
         config.rpc.readrpc_port = addr.port();
         config.rpc.admin_port = admin_addr.port();
+        if let Some(proxied_networks) = config.proxied_networks.as_mut() {
+            let proxied_addr = next_available_addr();
+            proxied_networks.host = match proxied_addr.ip() {
+                IpAddr::V4(ip) => ip,
+                IpAddr::V6(_) => Ipv4Addr::new(127, 0, 0, 1),
+            };
+            proxied_networks.grpc_port = proxied_addr.port();
+        };
 
         let config = Arc::new(config);
 
@@ -174,7 +190,15 @@ impl TestContext {
             config.clone(),
         );
 
-        let rpc = AgglayerImpl::new(Arc::new(service), rpc_service);
+        let proxied_networks = config
+            .proxied_networks
+            .as_ref()
+            .map(|pn| pn.networks.clone());
+        let allowed_networks = Box::new(move |incoming| match &proxied_networks {
+            None => true,
+            Some(pn) => !pn.contains(&incoming),
+        }) as _;
+        let rpc = AgglayerImpl::new(Arc::new(service), rpc_service, allowed_networks);
 
         RawRpcContext {
             rpc,
