f8cabb47cd837f9cb5caa7264ec58a5feac780f7
Keith Yeung <kyeung@polygon.technology>
2025-09-18 20:58:25 -0400

    refactor: use vkey hashing utilities from agglayer-primitives

--- crates/agglayer-contracts/src/aggchain.rs
+++ crates/agglayer-contracts/src/aggchain.rs
@@ -1,21 +1,10 @@
+pub use agglayer_primitives::vkey_hash::VKeyHash;
+
 use agglayer_primitives::{Address, U256};
 use alloy::primitives::Bytes;
 use tracing::error;
 
 use crate::{contracts::AggchainBase, L1RpcClient, L1RpcError};
-
-#[derive(PartialEq, Eq)]
-pub struct AggchainVkeyHash([u8; 32]);
-
-impl AggchainVkeyHash {
-    pub fn new(vkey: [u8; 32]) -> Self {
-        Self(vkey)
-    }
-
-    pub fn to_hex(&self) -> String {
-        hex::encode(self.0)
-    }
-}
 
 #[async_trait::async_trait]
 pub trait AggchainContract {
@@ -23,7 +12,7 @@ pub trait AggchainContract {
         &self,
         rollup_address: Address,
         aggchain_vkey_selector: u16,
-    ) -> Result<AggchainVkeyHash, L1RpcError>;
+    ) -> Result<VKeyHash, L1RpcError>;
 
     async fn get_aggchain_hash(
         &self,
@@ -46,7 +35,7 @@ where
         &self,
         rollup_address: Address,
         aggchain_vkey_selector: u16,
-    ) -> Result<AggchainVkeyHash, L1RpcError> {
+    ) -> Result<VKeyHash, L1RpcError> {
         let aggchain_selector = (((aggchain_vkey_selector as u32) << 16) | 1u32).to_be_bytes();
 
         let client = AggchainBase::new(rollup_address.into(), self.rpc.clone());
@@ -55,7 +44,7 @@ where
             .getAggchainVKey(alloy::primitives::FixedBytes(aggchain_selector))
             .call()
             .await
-            .map(|arg0| AggchainVkeyHash::new(arg0.0))
+            .map(|arg0| VKeyHash::from(arg0))
             .map_err(|error| {
                 error!(?error, "Unable to fetch the aggchain vkey");
 
