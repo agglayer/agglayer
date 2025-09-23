fae253d730085040d328872cb48ba3093fc46ef8
atanmarko <atanmarko@users.noreply.github.com>
2025-09-23 11:42:23 +0200

    feat: update contracts (#1053)
    Resolves https://github.com/agglayer/agglayer/issues/1046

--- crates/agglayer-contracts/src/contracts.rs
+++ crates/agglayer-contracts/src/contracts.rs
@@ -1,23 +1,35 @@
 #![allow(unused)]
 use alloy::{network::Ethereum, sol};
 
-sol!(
-    #[allow(missing_docs)]
-    #[allow(clippy::too_many_arguments)]
-    #[sol(rpc)]
-    #[derive(Debug, Eq, PartialEq)]
-    AggchainBase,
-    "src/contracts/AggchainBase.json"
-);
+mod aggchain_base {
+    use super::*;
+    sol!(
+        #[allow(missing_docs)]
+        #[allow(clippy::too_many_arguments)]
+        #[sol(rpc)]
+        #[derive(Debug, Eq, PartialEq)]
+        AggchainBase,
+        "src/contracts/AggchainBase.json"
+    );
+}
 
-sol!(
-    #[allow(missing_docs)]
-    #[allow(clippy::too_many_arguments)]
-    #[sol(rpc)]
-    #[derive(Debug, Eq, PartialEq)]
-    AgglayerGateway,
-    "src/contracts/AggLayerGateway.json"
-);
+mod agglayer_gateway {
+    use super::*;
+    sol!(
+        #[allow(missing_docs)]
+        #[allow(clippy::too_many_arguments)]
+        #[sol(rpc)]
+        #[derive(Debug, Eq, PartialEq)]
+        AgglayerGateway,
+        "src/contracts/AggLayerGateway.json"
+    );
+}
+
+// Re-export the contracts
+pub use aggchain_base::AggchainBase;
+// Use the IAggchainSigners from one of the modules to avoid duplication
+pub use aggchain_base::IAggchainSigners;
+pub use agglayer_gateway::AgglayerGateway;
 
 sol!(
     #[allow(missing_docs)]
