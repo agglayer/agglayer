5cfc484995fde5ea3334d5a3e13f4164c9250827
atanmarko <atanmarko@users.noreply.github.com>
2025-09-23 13:14:04 +0200

    chore: update provers dependency (#1059)

--- crates/pessimistic-proof-core/src/aggchain_data/aggchain_proof.rs
+++ crates/pessimistic-proof-core/src/aggchain_data/aggchain_proof.rs
@@ -18,7 +18,7 @@ pub struct AggchainProof {
 impl AggchainProof {
     /// Verifies the next proof in the sp1 buffer.
     pub fn verify_aggchain_proof(&self, constrained_values: &ConstrainedValues) {
-        let aggchain_proof_public_values = AggchainProofPublicValues {
+        let _aggchain_proof_public_values = AggchainProofPublicValues {
             prev_local_exit_root: constrained_values.initial_state_commitment.exit_root.into(),
             new_local_exit_root: constrained_values.final_state_commitment.exit_root.into(),
             l1_info_root: constrained_values.l1_info_root,
@@ -29,10 +29,13 @@ impl AggchainProof {
                 .commitment(IMPORTED_BRIDGE_EXIT_COMMITMENT_VERSION),
         };
 
-        // Panic upon invalid proof.
-        sp1_zkvm::lib::verify::verify_sp1_proof(
-            &self.aggchain_vkey,
-            &aggchain_proof_public_values.hash(),
-        );
+        #[cfg(target_os = "zkvm")]
+        {
+            // Panic upon invalid proof.
+            sp1_zkvm::lib::verify::verify_sp1_proof(
+                &self.aggchain_vkey,
+                &_aggchain_proof_public_values.hash(),
+            );
+        }
     }
 }
