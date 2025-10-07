770528627b45c72b20571d11b18abd237721000c
Leo Gaspard <lgaspard@polygon.technology>
2025-10-07 17:34:19 +0200

    feat: allow checking the pending state's pp root from network task

--- crates/agglayer-certificate-orchestrator/src/network_task.rs
+++ crates/agglayer-certificate-orchestrator/src/network_task.rs
@@ -3,13 +3,14 @@ use std::sync::Arc;
 use agglayer_clock::ClockRef;
 use agglayer_storage::{
     columns::latest_settled_certificate_per_network::SettledCertificate,
-    stores::{PendingCertificateReader, PendingCertificateWriter, StateReader, StateWriter},
+    stores::{state, PendingCertificateReader, PendingCertificateWriter, StateReader, StateWriter},
 };
 use agglayer_types::{
     primitives::{Digest, Hashable as _},
     CertificateId, CertificateIndex, CertificateStatusError, EpochNumber, Height,
     LocalNetworkStateData, NetworkId, SettlementTxHash,
 };
+use pessimistic_proof::core::commitment::PessimisticRootCommitmentVersion;
 use tokio::sync::{broadcast, mpsc, oneshot};
 use tokio_util::sync::CancellationToken;
 use tracing::{debug, error, info, warn};
@@ -453,5 +454,31 @@ where
             .map_err(|e| Error::InternalError(format!("Certificate task panicked: {e}")))?;
 
         Ok(())
+    }
+
+    fn pending_pessimistic_root(
+        &self,
+        height: Height,
+        version: PessimisticRootCommitmentVersion,
+    ) -> Digest {
+        let state_commitment = self.local_state.get_roots();
+        let pp_commitment_values =
+            pessimistic_proof::core::commitment::PessimisticRootCommitmentValues {
+                height: height.as_u64(),
+                origin_network: self.network_id,
+
+                ler_leaf_count: state_commitment.ler_leaf_count,
+                balance_root: state_commitment.balance_root.into(),
+                nullifier_root: state_commitment.nullifier_root.into(),
+            };
+        pp_commitment_values.compute_pp_root(version)
+    }
+
+    fn is_pending_pessimistic_root(&self, settled_pp_root: Digest, height: Height) -> bool {
+        let computed_v2 =
+            self.pending_pessimistic_root(height, PessimisticRootCommitmentVersion::V2);
+        let computed_v3 =
+            self.pending_pessimistic_root(height, PessimisticRootCommitmentVersion::V3);
+        settled_pp_root == computed_v2 || settled_pp_root == computed_v3
     }
 }
