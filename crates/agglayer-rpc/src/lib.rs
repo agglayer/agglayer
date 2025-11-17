1970d2da8ad9f8ccfc8101f4d8b7b1f02790a4c2
Simon Paitrault <spaitrault@polygon.technology>
2025-11-17 15:35:59 +0100

    feat: allowing disabling networks (#1160)
    CONFIG-CHANGE: Short description of the configuration changes.
      More details can be added in the description.
      Link to configuration file example if applicable.
      Link to migration guide if applicable.
      Multiple configuration changes can be listed.
    
    BREAKING-CHANGE: Short description of the breaking changes.
      More details can be added in the description.
      Multiple lines can be used, with **markdown** formatting if desired.
      Multiple breaking changes can be listed.
    
    ---------
    
    Signed-off-by: Simon Paitrault <spaitrault@polygon.technology>

--- crates/agglayer-rpc/src/lib.rs
+++ crates/agglayer-rpc/src/lib.rs
@@ -430,6 +430,7 @@ where
 
     /// Assemble the current information of the specified network from
     /// the data in various sources.
+    #[instrument(skip(self))]
     pub fn get_network_info(
         &self,
         network_id: NetworkId,
@@ -630,7 +631,25 @@ where
             }
         }
 
+        let network_is_disabled = self
+            .state
+            .is_network_disabled(&network_id)
+            .map_err(|error| {
+                error!(
+                    ?error,
+                    "Failed to check if network {network_id} is disabled in storage"
+                );
+                GetNetworkInfoError::InternalError {
+                    network_id,
+                    source: error.into(),
+                }
+            })?;
+
         match network_info.latest_pending_status {
+            _ if network_is_disabled => {
+                // If the network is disabled in storage, mark it as disabled
+                network_info.network_status = NetworkStatus::Disabled;
+            }
             None => {
                 // No pending certificate means the network status is unknown
                 network_info.network_status = NetworkStatus::Unknown;
