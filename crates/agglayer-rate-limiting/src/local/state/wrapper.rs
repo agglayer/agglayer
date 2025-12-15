6ffd7dc0c888f208eac346253b19fc95dd032648
sashass1315 <sashass1315@gmail.com>
2025-12-15 19:41:53 +0200

    ilence dead_code warning for state raw accessor

--- crates/agglayer-rate-limiting/src/local/state/wrapper.rs
+++ crates/agglayer-rate-limiting/src/local/state/wrapper.rs
@@ -48,6 +48,7 @@ impl<S: RawState> State<S> {
     }
 
     /// Get non-modifying access to the underlying raw state.
+    #[allow(dead_code)]
     pub fn raw(&self) -> &S {
         &self.raw
     }
