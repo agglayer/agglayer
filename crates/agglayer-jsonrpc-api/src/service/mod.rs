4a795fa429e3c97e4837f708ff993a4854f03a40
GarmashAlex <garmasholeksii@gmail.com>
2026-01-13 01:09:55 +0300

    fix

--- crates/agglayer-jsonrpc-api/src/service/mod.rs
+++ crates/agglayer-jsonrpc-api/src/service/mod.rs
@@ -132,15 +132,18 @@ where
     pub async fn get_tx_status(&self, hash: B256) -> Result<TxStatus, TxStatusError> {
         debug!("Received request to get transaction status for l1 tx hash {hash}");
 
-        let receipt = self.kernel.check_tx_status(hash).await.map_err(|error| {
-            error!(
-                ?error,
-                "Failed to get transaction status for l1 tx hash {hash}"
-            );
-            TxStatusError::StatusCheck(error)
-        })?;
-
-        let receipt = receipt.ok_or_else(|| TxStatusError::TxNotFound { hash })?;
+        let receipt = self
+            .kernel
+            .check_tx_status(hash)
+            .await
+            .map_err(|error| {
+                error!(
+                    ?error,
+                    "Failed to get transaction status for l1 tx hash {hash}"
+                );
+                TxStatusError::StatusCheck(error)
+            })?
+            .ok_or_else(|| TxStatusError::TxNotFound { hash })?;
 
         let current_block = self
             .kernel
